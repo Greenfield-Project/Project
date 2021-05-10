41139a0c25d38b514bb7bfc41ee0924be72bcc89
BSsAmal <bsassiamal@gmail.com>
2021-05-10 17:31:51 +0100

    project

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          
+      };
+  }
+  render() {
+    return (
+        <div className="flex-container">
+           <div>hello</div>
+        </div>
+    );
+}
+}
+export default App 
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(port, () => {
+  console.log(`cats are available on http://localhost:${port}`);
+});
